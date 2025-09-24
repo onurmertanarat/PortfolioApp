fb9b451203cb05817630a9f1f7c5947c8fdbd5d1
onurmertanarat <onurmert7213@gmail.com>
2025-09-24 14:32:00 +0300

    feat: Secure SECRET_KEY and DEBUG using .env file

--- portfolio_app/portfolio_app/settings.py
+++ portfolio_app/portfolio_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fmq5!_jnyzl44ybc5(aym6_$o!5#w%#jylmkn(+hdzm-x8z6x('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
